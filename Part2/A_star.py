8ef5c2f2a392cd155330eb5327cdea4543bf565e
larissa <larissafelicianasilva@gmail.com>
2021-07-05 14:11:54 -0300

    Fixing cost calculation

--- Part2/A_star.py
+++ Part2/A_star.py
@@ -5,12 +5,12 @@ actions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 
 
 class Node():
-    def __init__(self, state, parent=None, energy_cost=0, path_cost=0):
+    def __init__(self, state, parent=None, energy_cost=0, heuristic_cost=0):
         self.state = state
         self.parent = parent
-        self.energy_cost = energy_cost
-        self.path_cost = path_cost
-        self.cost = energy_cost + path_cost
+        self.energy_cost = energy_cost + (parent.energy_cost if parent else 0)
+        self.heuristic_cost = heuristic_cost
+        self.cost = self.energy_cost + heuristic_cost
 
     def __str__(self):
         return str(self.state)
@@ -36,13 +36,13 @@ def search(map, agent_position, food_position):
             return solution(node)
 
         else:
-            expand_node(map, node, agent_position)
+            expand_node(map, node, food_position)
             if not frontier:
                 raise Exception("Solucao nao encontrada")
 
-def path_cost(agent_position, current_position):
-    return (abs(agent_position[0] - current_position[0]) +
-            abs(agent_position[1] - current_position[1]))
+def heuristic_cost(food_position, current_position):
+    return (abs(food_position[0] - current_position[0]) +
+            abs(food_position[1] - current_position[1]))
 
 def energy_cost(map, current_position):
     ground_cost = {
@@ -65,14 +65,14 @@ def solution(node):
             node = node.parent
     return solution_path
 
-def expand_node(map, node, agent_position):
+def expand_node(map, node, food_position):
     global frontier
     global explored_set
 
     for action in actions:
         state = (node.state[0] + action[0], node.state[1] + action[1])
         if check_valid_node(state, map):
-            child_node = Node(state, node, energy_cost(map, state), path_cost(agent_position, state))
+            child_node = Node(state, node, energy_cost(map, state), heuristic_cost(food_position, state))
             frontier.append((child_node.cost, child_node))
     frontier = sorted(frontier, key=lambda x: x[0])
 
